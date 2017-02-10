c913b473357470ee47e0dea4c463965e7ff9b15b
Jose Manuel Delicado <jmdaweb@hotmail.com>
2017-02-10 19:11:42 +0100

    The parser imports the symbols tree

--- src/com/semv/practica1/syntactic/parser.java
+++ src/com/semv/practica1/syntactic/parser.java
@@ -5,6 +5,7 @@
 
 package com.semv.practica1.syntactic;
 
+import com.semv.practica1.symbols_table.ScopeTree;
 import java_cup.runtime.XMLElement;
 
 /** CUP v0.11b 20160615 (GIT 4ac7450) generated parser.
