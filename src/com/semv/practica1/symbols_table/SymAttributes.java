4497d83b53cfe419aec7882b5aeda46719462e21
Angel <info@angelfrancisco.es>
2017-03-02 22:34:36 +0100

    Update SymAttributes class

--- src/com/semv/practica1/symbols_table/SymAttributes.java
+++ src/com/semv/practica1/symbols_table/SymAttributes.java
@@ -2,19 +2,19 @@ package com.semv.practica1.symbols_table;
 
 public class SymAttributes {
 	
-	private int type;
+	private String type;
 	private int row;
 	private int col;
 	private String name;
 	
-	public SymAttributes (int type, int row, int col, String name) {
+	public SymAttributes (String type, int row, int col, String name) {
 		this.type = type;
 		this.row = row;
 		this.col = col;
 		this.name=name;
 	}
 	
-	public int getType () {
+	public String getType () {
 		return this.type;
 	}
 	
